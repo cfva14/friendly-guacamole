7a880aab24cc53f4742e38cf1ddd5a38af2bb6c8
Carlos Valencia <cfva14@gmail.com>
2018-06-09 15:54:55 -0700

    Added Resource and Status classes for data requests

---
+++ src/main/java/io/github/cfva14/friendlyguacamole/data/Resource.kt
@@ -0,0 +1,25 @@
+package io.github.cfva14.friendlyguacamole.data
+
+import io.github.cfva14.friendlyguacamole.data.Status.*
+
+/**
+ * Created by Carlos Valencia on 6/9/18
+ */
+
+data class Resource<out T>(val status: Status, val data: T?, val message: String?) {
+
+    companion object {
+        fun <T> success(data: T?): Resource<T> {
+            return Resource(SUCCESS, data, null)
+        }
+
+        fun <T> error(message: String, data: T?): Resource<T> {
+            return Resource(ERROR, data, message)
+        }
+
+        fun <T> loading(data: T?): Resource<T> {
+            return Resource(LOADING, data, null)
+        }
+    }
+
+}
---
+++ src/main/java/io/github/cfva14/friendlyguacamole/data/Status.kt
@@ -0,0 +1,11 @@
+package io.github.cfva14.friendlyguacamole.data
+
+/**
+ * Created by Carlos Valencia on 6/9/18
+ */
+
+enum class Status {
+    SUCCESS,
+    ERROR,
+    LOADING
+}
